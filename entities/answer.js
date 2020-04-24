da30c12aa86867049cab36a88afa7cf70565277f
Alessandro <alessandrodalgobbo@gmail.com>
2020-04-24 19:28:20 +0200

    initial commit

---
+++ entities/answer.js
@@ -0,0 +1,10 @@
+class Answer {
+    constructor(text, points){
+        this.text = text;
+        this.points = points;
+    }
+}
+
+module.exports = {
+    Answer
+};
---
+++ entities/question.js
@@ -0,0 +1,10 @@
+class Question {
+    constructor(text, answer){
+        this.text = text;
+        this.answer = answer;
+    }
+}
+
+module.exports = {
+    Question
+};
